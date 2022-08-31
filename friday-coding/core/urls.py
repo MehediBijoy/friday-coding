2d57628da6bf6fac4773fe17df7b219c060491f6
Mehedi Hasan <mehedibubt34@gmail.com>
2022-08-31 15:08:46 +0600

    Delete friday-coding directory

--- friday-coding/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import TransferBalance, TransactionHistory
-
-urlpatterns = [
-    path('transfer_balance/', TransferBalance.as_view()),
-    path('transaction_history/', TransactionHistory.as_view()),
-]
--- friday-coding/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import Registration
-
-urlpatterns = [
-    path('registration/', Registration.as_view())
-]
